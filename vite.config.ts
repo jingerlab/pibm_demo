2e985f2c86aeec4e287fde300ef09b6957f8d3c8
jingerlab <jingerlab.in@gmail.com>
2026-07-19 05:29:23 +0530

    Plotly Test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite'
+// setup/main.ts
+import Plotly from 'plotly.js-dist-min';
+(window as any).Plotly = Plotly;
 
 export default defineConfig({
 	// Must have BOTH a leading and a trailing slash!
